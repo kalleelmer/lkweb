d15c538d98d34dd0e184236bfc98f3afa677f60d
Kristoffer <kristoffer.ipod@gmail.com>
2017-06-30 16:17:56 +0200

    Början på lista till föreställninger
    Är det föreställningar eller gems som ska säljas? bra fråga…

--- pages/shows/ShowsCtrl.js
+++ pages/shows/ShowsCtrl.js
@@ -1,7 +1,22 @@
 var module = angular.module("lkticket.admin");
 
+var shows = [
+    { name: 'Azurite', price: 2.95 },
+    { name: 'Bloodstone', price: 5.95 },
+    { name: 'Zircon', price: 3.95 }
+  ];
+
 var ShowsCtrl = function($scope, $http, User) {
-	// TODO
+
+  this.shows = shows;
+
 }
+
+module.directive("showPanel", function() {
+  return {
+    restrict: "E",
+    templateUrl: "/pages/shows/show-panel.html"
+  };
+});
 
 module.controller("ShowsCtrl", ShowsCtrl);
