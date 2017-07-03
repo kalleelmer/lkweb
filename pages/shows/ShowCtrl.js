2488c8486b803775de3d88c38710885760fdd9b0
Kristoffer <kristoffer.ipod@gmail.com>
2017-07-03 13:18:36 +0200

    Sorterar visningen av föreställningar på datum

--- pages/shows/ShowCtrl.js
+++ pages/shows/ShowCtrl.js
@@ -1,19 +1,45 @@
 var module = angular.module("lkticket.admin");
 
-var ShowCtrl = function($scope, $http, User, $routeParams, Core) {
+var catagories = [{name : "Barnplats", Prices: {barn: 30, fribiljett: 0}},
+{anme : "Stol", prices: {student: 60, vuxen: 80, fribiljett: 0}},
+{name : "Parkett", prices: {student: 60, vuxen: 80, fribiljett: 0}}];
+
+var ShowCtrl = function($filter, $scope, $http, User, $routeParams, Core) {
+
+	$scope.formatDate = function(date){
+		return date.replace(" ", "T");
+	}
+
+	$scope.sortShows = function(shows) {
+		this.sortedShows = {};
+
+		for (var i in shows){
+
+			var dateKey = $filter('date')($scope.formatDate(shows[i].start), "dd/MM/yyyy");
+
+			if(!this.sortedShows[dateKey]){
+				this.sortedShows[dateKey] = [];
+			}
+			this.sortedShows[dateKey].push(shows[i]);
+		}
+		console.log(this.sortedShows);
+		$scope.performancesByDate = this.sortedShows;
+
+	}
 
 	$scope.id = $routeParams.id;
+
 	Core.get("/admin/shows/" + $scope.id + "/performances").then(
 		function(response) {
-			$scope.performances = response.data;
+
+			$scope.sortShows(response.data);
 		}, function(response) {
 			alert("Ett problem uppstod");
 		});
 
 
-		$scope.formatDate = function(date){
-			return date.replace(" ", "T");
-		}
+
+
 
 }
 
