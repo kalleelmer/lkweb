a3d05d60ae7de7cfee253b1896ea773a63279ddb
Kristoffer <kristoffer@beontime.se>
2017-07-04 15:33:42 +0200

    Hämtar priser från databasen och lägger ut dem i tabellen
    Fortfarande buggar angående uppdatera databasen, metoder finns på
    plats, men databasen tar inte emot requestet

--- pages/shows/ShowCtrl.js
+++ pages/shows/ShowCtrl.js
@@ -1,71 +1,112 @@
 var module = angular.module("lkticket.admin");
 
 var ShowCtrl = function($filter, $scope, $http, User, $routeParams, Core) {
-	var ctrl = this;
+  var ctrl = this;
 
-	$scope.formatDate = function(date) {
-		return date.replace(" ", "T");
-	};
+  $scope.formatDate = function(date) {
+    return date.replace(" ", "T");
+  };
 
-	$scope.id = $routeParams.id;
+  $scope.id = $routeParams.id;
 
-	$scope.addPerf = function(date) {
-		var time = prompt("Ange klockslag (HH:mm)");
-		if (!time) {
-			return;
-		}
-		$scope.show.dates[date].push({
-			start : date + " " + time
-		});
-	}
+  $scope.addPerf = function(date) {
+    var time = prompt("Ange klockslag (HH:mm)");
+    if (!time) {
+      return;
+    }
+    $scope.show.dates[date].push({
+      start: date + " " + time
+    });
+  }
 
-	$scope.addDate = function() {
-		$scope.show.dates[$scope.newDate] = [];
-		$scope.newDate = "";
-	};
+  $scope.addDate = function() {
+    $scope.show.dates[$scope.newDate] = [];
+    $scope.newDate = "";
+  };
 
-	Core.get("/admin/shows/" + $scope.id).then(function(response) {
-		$scope.show = response.data;
-		ctrl.loadShowData();
-	}, function(response) {
-		alert("Kunde inte hämta nöje: " + response.status);
-	});
+  Core.get("/admin/shows/" + $scope.id).then(function(response) {
+    $scope.show = response.data;
+    ctrl.loadShowData();
+  }, function(response) {
+    alert("Kunde inte hämta nöje: " + response.status);
+  });
 
-	ctrl.loadShowData = function() {
-		Core.get("/admin/shows/" + $scope.id + "/performances").then(
-			function(response) {
-				var dates = {};
-				for ( var i in response.data) { // Group by date
-					var show = response.data[i];
-					var key = show.start.substring(0, 10);
-					dates[key] = dates[key] ? dates[key] : [];
-					dates[key].push(show);
-				}
-				$scope.show.dates = dates;
-			}, function(response) {
-				alert("Kunde inte hämta föreställningar: " + response.status);
-			});
-		Core.get("/admin/shows/" + $scope.id + "/rates").then(
-			function(response) {
-				$scope.show.rates = response.data;
-			}, function(response) {
-				alert("Kunde inte hämta biljettyper: " + response.status);
-			});
-		Core.get("/admin/shows/" + $scope.id + "/categories").then(
-			function(response) {
-				$scope.show.categories = response.data;
-			}, function(response) {
-				alert("Kunde inte hämta platstyper: " + response.status);
-			});
-	}
+  ctrl.loadShowData = function() {
+    Core.get("/admin/shows/" + $scope.id + "/performances").then(
+      function(response) {
+        var dates = {};
+        for (var i in response.data) { // Group by date
+          var show = response.data[i];
+          var key = show.start.substring(0, 10);
+          dates[key] = dates[key] ? dates[key] : [];
+          dates[key].push(show);
+        }
+        $scope.show.dates = dates;
+      },
+      function(response) {
+        alert("Kunde inte hämta föreställningar: " + response.status);
+      });
 
-	$scope.postCategory = function(category) {
-		Core.post("/admin/shows/" + $scope.id + "/categories", category);
-	};
+    Core.get("/admin/shows/" + $scope.id + "/rates").then(
+      function(response) {
+        $scope.show.rates = response.data;
 
-	$scope.postRate = function(rate) {
-		Core.post("/admin/shows/" + $scope.id + "/rates", rate);
-	};
+        Core.get("/admin/shows/" + $scope.id + "/categories").then(
+          function(response) {
+            $scope.show.categories = response.data;
+
+            for (var i in $scope.show.categories) {
+              var category = $scope.show.categories[i];
+              getPrices(category);
+            }
+
+          },
+          function(response) {
+            alert("Kunde inte hämta platstyper: " + response.status);
+          })
+
+      },
+      function(response) {
+        alert("Kunde inte hämta biljettyper: " + response.status);
+      });
+
+  }
+
+  var getPrices = function(category) {
+    Core.get("/admin/shows/" + $scope.id + "/categories/" + category.id + "/prices").then(
+      function(response) {
+        category.prices = [];
+
+        for (var i in response.data) {
+          var price = response.data[i];
+          category.prices[price.rate_id] = price;
+        }
+
+        console.log($scope.show);
+
+      },
+      function(response) {
+        alert("Kunde inte hämta priserna: " + response.status);
+      });
+  }
+
+  $scope.postCategory = function(category) {
+    Core.post("/admin/shows/" + $scope.id + "/categories", category);
+  };
+
+  $scope.postRate = function(rate) {
+    Core.post("/admin/shows/" + $scope.id + "/rates", rate);
+  };
+
+  $scope.postPrice = function(newPrice) {
+
+    console.log(newPrice);
+
+    Core.post("/admin/shows/" + $scope.id + "/categories/" + newPrice.category_id + "/prices", {
+      rate_id: newPrice.rate_id,
+      price: +newPrice.price
+    });
+  };
 
 };
 
