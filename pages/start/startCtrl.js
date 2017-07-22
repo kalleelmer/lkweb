5032655e595de3f807689bfe812558bc9276ff48
Kristoffer <kristoffer.nordstrom@outlook.com>
2017-07-22 15:00:48 +0200

    hämtar en xkdc och visar på startsidan

--- pages/start/startCtrl.js
+++ pages/start/startCtrl.js
@@ -1,6 +1,17 @@
 var module = angular.module("lkticket.admin");
 
-var ShowCtrl = function($filter, $scope, $http, User, $routeParams, Core) {
+var ShowCtrl = function($filter, $scope, $http, User, $routeParams, Core, $sce) {
+
+
+
+  var url = "http://dynamic.xkcd.com/api-0/jsonp/comic/" + Math.floor((Math.random() * 1800) + 1);;
+
+  $http.jsonp($sce.trustAsResourceUrl(url)).then(function(data) {
+    console.log(data.data);
+    $scope.comicStrip = data.data.img;
+  }, function (data) {
+    console.log(data);
+  });
 
 }
 
