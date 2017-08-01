caac647976b6ad6a6843dbbc8decb93f58484771
Kristoffer <kristoffer.ipod@gmail.com>
2017-08-01 20:42:37 +0200

    filer rensade

--- pages/cart/CartCtrl.js
+++ pages/cart/CartCtrl.js
@@ -1,11 +1,7 @@
 var module = angular.module("lkticket.admin");
 
 var UserCtrl = function(Core, $scope, $routeParams) {
-	Core.get("/admin/users/" + $routeParams.id).then(function(response) {
-		$scope.user = response.data;
-	}, function(response) {
-		alert("Kunde inte hämta användaren: " + response.status);
-	});
+
 }
 
 module.controller("UserCtrl", UserCtrl);
--- pages/show/ShowCtrl.js
+++ pages/show/ShowCtrl.js
@@ -2,35 +2,6 @@ var module = angular.module("lkticket.admin");
 
 var ShowsCtrl = function($scope, $http, User, Core) {
 
-	Core.get("/admin/shows").then(function(response) {
-		$scope.shows = response.data;
-	}, function(response) {
-		alert("Kunde inte hämta nöjan: " + response.status);
-	});
-
-	$scope.toggleEditMode = function(show) {
-		show.editToggle = !show.editToggle;
-	}
-
-	$scope.editNameOfShow = function(show) {
-		// TODO Lösa riktigt mot databasen
-		$scope.toggleEditMode(show);
-	}
-
-	$scope.deleteShow = function(show) {
-		// TODO Lösa riktigt mot databasen
-		$scope.shows.splice($scope.shows.indexOf(show), 1);
-	}
-
-	$scope.addNewShow = function(show) {
-		Core.post("/admin/shows", show).then(function(response){
-			$scope.shows.push(response.data);
-			show.name = "";
-		}, function(response) {
-			alert("kunde inte lägga till nöje: ", response.status);
-		});
-	}
-
 }
 
 module.controller("ShowsCtrl", ShowsCtrl);
