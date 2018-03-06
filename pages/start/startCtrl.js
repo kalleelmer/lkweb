9771d3e4d83ce6ae1e90f3b6647a05dab53e984b
kristoffer <kristoffer.nordstrom@outlook.com>
2018-03-06 20:25:32 +0100

    Kan hämta från middle

--- pages/start/startCtrl.js
+++ pages/start/startCtrl.js
@@ -4,6 +4,12 @@ var ShowCtrl = function($filter, $scope, $http, User, $routeParams, Core, $sce) 
 
 
 
+  Core.get("/shows").then(function(response) {
+    $scope.shows = response.data;
+  }, function(error) {
+    console.log(error);
+  });
+
 }
 
 module.controller("StartCtrl", ShowCtrl);
