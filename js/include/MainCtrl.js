4ba0d4d6b338b9e1142a7cc97fae0c8dbccb43a6
kristoffer <kristoffer.nordstrom@outlook.com>
2018-03-08 20:20:19 +0100

    Hämtar menu

--- js/include/MainCtrl.js
+++ js/include/MainCtrl.js
@@ -3,6 +3,12 @@ var module = angular.module("lkticket.webb");
 var MainCtrl = function($scope, User, $route, Core) {
 	var ctrl = this;
 
+	Core.get("/shows").then(function(response) {
+		$scope.shows = response.data;
+	}, function(error) {
+		console.log(error);
+	});
+
 }
 
 module.controller("MainCtrl", MainCtrl);
