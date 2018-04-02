854dee41dad4fd6ccb1d7ba88e83c3c558809cd5
Kalle Elmér <kalle@silvertejp.nu>
2018-04-02 17:39:46 +0200

    Ladda föreställningar efter nöjet för att undvika krash.

--- pages/show/ShowCtrl.js
+++ pages/show/ShowCtrl.js
@@ -1,29 +1,35 @@
 var module = angular.module("lkticket.webb");
 
 var ShowCtrl = function($scope, $routeParams, $http, User, Core) {
+	var ctrl = this;
 
-  Core.get("/shows/" + $routeParams.id).then(function(response) {
-    $scope.show = response.data;
-    $scope.show.dates = [];
-  }, function(error) {
-    console.log(error);
-  });
+	Core.get("/shows/" + $routeParams.id).then(function(response) {
+		$scope.show = response.data;
+		$scope.show.dates = [];
+		ctrl.reloadPerformances();
+	}, function(error) {
+		console.log(error);
+	});
 
-  Core.get("/shows/" + $routeParams.id + "/performances").then(function(response) {
+	ctrl.reloadPerformances = function() {
+		Core.get("/shows/" + $routeParams.id + "/performances").then(
+			function(response) {
 				var dates = {};
-        if (response.data.length > 0){
-          for ( var i in response.data) { // Group by date
-    				var perf = response.data[i];
-    				var key = new Date(perf.start).toISOString().substring(0, 10);
+				if (response.data.length > 0) {
+					for ( var i in response.data) { // Group by date
+						var perf = response.data[i];
+						var key = new Date(perf.start).toISOString().substring(
+							0, 10);
 
-    				dates[key] = dates[key] ? dates[key] : [];
-    				dates[key].push(perf);
-    			}
-          $scope.show.dates = dates;
-        }
+						dates[key] = dates[key] ? dates[key] : [];
+						dates[key].push(perf);
+					}
+					$scope.show.dates = dates;
+				}
 			}, function(response) {
 				alert("Kunde inte hämta föreställningar: " + response.status);
 			});
+	}
 }
 
 module.controller("ShowCtrl", ShowCtrl);
