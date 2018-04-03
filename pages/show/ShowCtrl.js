2392ded29f1f61fdd21aba8fa62d9445fcf9e336
kristoffer <kristoffer.nordstrom@outlook.com>
2018-04-03 13:03:22 +0200

    visar antal platser kvar totalt

--- pages/show/ShowCtrl.js
+++ pages/show/ShowCtrl.js
@@ -18,6 +18,7 @@ var ShowCtrl = function($scope, $routeParams, $http, User, Core) {
 				if (response.data.length > 0) {
 					for ( var i in response.data) { // Group by date
 						var perf = response.data[i];
+						ctrl.loadAvilivility(perf);
 						var key = new Date(perf.start).toISOString().substring(
 							0, 10);
 
@@ -25,11 +26,28 @@ var ShowCtrl = function($scope, $routeParams, $http, User, Core) {
 						dates[key].push(perf);
 					}
 					$scope.show.dates = dates;
+					console.log(dates);
 				}
 			}, function(response) {
 				alert("Kunde inte hämta föreställningar: " + response.status);
 			});
 	}
+
+	ctrl.loadAvilivility = function(perf) {
+			console.log(perf);
+			Core.get("/performances/" + perf.id).then(function(response) {
+				var available = 0;
+				console.log(response.data);
+				for (var key in response.data.availability) {
+					available+= response.data.availability[key].available;
+				}
+
+				perf.available = available;
+			}, function(error) {
+				console.log(error);
+			});
+	}
+
 }
 
 module.controller("ShowCtrl", ShowCtrl);
