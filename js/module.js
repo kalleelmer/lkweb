1ff84cf9d15139bf8a29be7bf8d962ceac6d4c79
kristoffer <kristoffer.nordstrom@outlook.com>
2018-04-12 20:24:49 +0200

    analytics finns

--- js/module.js
+++ js/module.js
@@ -1,19 +1,26 @@
 var module = angular.module("lkticket.webb", ["ngRoute", "xeditable",
-  "ngFileUpload", "angular.filter", "ui-notification"
+	"ngFileUpload", "angular.filter", "ui-notification", 'angular-google-analytics'
 ]);
 
 module.run(function(editableOptions) {
-  editableOptions.theme = 'bs3';
+	editableOptions.theme = 'bs3';
 });
 
 module.config(function(NotificationProvider) {
-  NotificationProvider.setOptions({
-    delay: 5000,
-    startTop: 67,
-    startRight: 10,
-    verticalSpacing: 20,
-    horizontalSpacing: 20,
-    positionX: 'right',
-    positionY: 'bottom'
-  });
+	NotificationProvider.setOptions({
+		delay: 5000,
+		startTop: 67,
+		startRight: 10,
+		verticalSpacing: 20,
+		horizontalSpacing: 20,
+		positionX: 'right',
+		positionY: 'bottom'
+	});
 });
+
+module.config(['AnalyticsProvider', function (AnalyticsProvider) {
+  console.log("conf analytics");
+   AnalyticsProvider.setAccount('UA-117433868-1');  //UU-XXXXXXX-X should be your tracking code
+}]).run(['Analytics', function(Analytics) {
+  console.log("starting analytics");
+}]);
