9d14cb7ff7d2b5a67b250f73fa4068673a6562bb
Kalle Elmér <kalle@silvertejp.nu>
2018-04-16 18:44:41 +0200

    Hantera specialdatum för entréer i föreställningslistan.

--- pages/show/ShowCtrl.js
+++ pages/show/ShowCtrl.js
@@ -14,38 +14,35 @@ var ShowCtrl = function($scope, $routeParams, $http, User, Core) {
 	ctrl.reloadPerformances = function() {
 		Core.get("/shows/" + $routeParams.id + "/performances").then(
 			function(response) {
-				var dates = {};
-				if (response.data.length > 0) {
-					for ( var i in response.data) { // Group by date
-						var perf = response.data[i];
-						ctrl.loadAvilivility(perf);
-						var key = new Date(perf.start).toISOString().substring(
-							0, 10);
-
-						dates[key] = dates[key] ? dates[key] : [];
-						dates[key].push(perf);
+				$scope.performances = response.data;
+				for ( var i in response.data) { // Group by date
+					var perf = response.data[i];
+					if (perf.title) {
+						perf.date = null;
+					} else {
+						perf.date = new Date(perf.start).toISOString()
+							.substring(0, 10);
 					}
-					$scope.show.dates = dates;
-					console.log(dates);
+					ctrl.loadAvailability(perf);
 				}
 			}, function(response) {
 				alert("Kunde inte hämta föreställningar: " + response.status);
 			});
 	}
 
-	ctrl.loadAvilivility = function(perf) {
-			console.log(perf);
-			Core.get("/performances/" + perf.id).then(function(response) {
-				var available = 0;
-				console.log(response.data);
-				for (var key in response.data.availability) {
-					available+= response.data.availability[key].available;
-				}
+	ctrl.loadAvailability = function(perf) {
+		console.log(perf);
+		Core.get("/performances/" + perf.id).then(function(response) {
+			var available = 0;
+			console.log(response.data);
+			for ( var key in response.data.availability) {
+				available += response.data.availability[key].available;
+			}
 
-				perf.available = available;
-			}, function(error) {
-				console.log(error);
-			});
+			perf.available = available;
+		}, function(error) {
+			console.log(error);
+		});
 	}
 
 }
